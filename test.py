04ef76fe1784d205bd245afe660fbced07c0a1b1
Heesu Jeong <huissppea@gmail.com>
2023-07-28 15:34:28 +0900

    test 파일 수정

--- test.py
+++ test.py
@@ -4,14 +4,38 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+app.config['JSON_AS_ASCII'] = False
+
 class test(Resource):
     def get(self):
         return {
-                "image": '이미지에요',
-                "lengthTime": '영상길이에요',
-                "title": '제목이에요',
-                "channelName" : '채널이름이에요'
-                }
+	"YouTubeBigBoxs": [{
+			"image": "https: //i.ytimg.com/vi/3yluDg_GnfE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-NoAzWEK-9CdzznuxAmXv6r0Vdw",
+			"lengthTime": "9: 33",
+			"title": "아침을 시작하는 5분 명상 | 아침명상, 아침 스트레칭",
+			"channelName": "에일린 mind yoga"
+		},
+		{
+			"image": "https: //i.E/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-NoAzWEK-9CdzznuxAmXv6r0Vdw",
+			"lengthTime": "9: 33",
+			"title": "2아침을 시작하는 5분 명상 | 아침명상, 아침 스트레칭",
+			"channelName": "에일린 mind yoga"
+		},
+		{
+			"image": "https: //i.ytimg.com/vi/3yluDg_GnfE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-NoAzWEK-9CdzznuxAmXv6r0Vdw",
+			"lengthTime": "9: 33",
+			"title": "3아침을 시작하는 5분 명상 | 아침명상, 아침 스트레칭",
+			"channelName": "에일린 mind yoga"
+		},
+		{
+			"image": "https: //i.ytimg.com/vi/3yluDg_GnfE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-NoAzWEK-9CdzznuxAmXv6r0Vdw",
+			"lengthTime": "9: 33",
+			"title": "4아침을 시작하는 5분 명상 | 아침명상, 아침 스트레칭",
+			"channelName": "에일린 mind yoga"
+		}
+	]
+
+}
         
 api.add_resource(test, '/test')
 app.run(debug=False)
